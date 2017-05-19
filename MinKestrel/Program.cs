4a37b4c3ed050e566b7f15bae3d7443f78586e9a
Mike Harder <mharder@microsoft.com>
2017-05-19 11:57:34 -0700

    Minimal Kestrel app

---
+++ MinKestrel/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MinKestrel
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app => app.Run(async (context) =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                }))
+                .UseUrls("http://*:5000")
+                .Build()
+                .Run();
+        }
+    }
+}
